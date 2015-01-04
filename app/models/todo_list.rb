4d4b2f246fadea330594d242b3cea0cfa5877098
John Carmichael <jlcarmic@gmail.com>
2015-01-04 13:04:31 -0500

    Showing completed vs incomplete items

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,4 +3,12 @@ class TodoList < ActiveRecord::Base
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }
+
+  def has_completed_items?
+    todo_items.complete.size > 0
+  end
+
+    def has_incomplete_items?
+    todo_items.incomplete.size > 0
+  end
 end
